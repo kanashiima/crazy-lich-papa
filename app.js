eec9fd421c7e5c4a6577199d8cf437f8d07e77d9
kanashiima <tehphob@gmail.com>
2023-02-24 19:42:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const { ObjectId } = require('mongodb')
 const PORT = process.env.PORT || 3000;
 const herokuVar = process.env.HEROKU_NAME || "local Barry"
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const MONGO_URI = "mongodb+srv://barry:GMSk9usexg5A8p5Q@cluster0.taug6.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
@@ -18,9 +17,7 @@ async function cxnDB(){
   try{
     client.connect; 
     const collection = client.db("chillAppz").collection("drinkz");
-    // const collection = client.db("papa").collection("dev-profiles");
     const result = await collection.find().toArray();
-    //const result = await collection.findOne(); 
     console.log("cxnDB result: ", result);
     return result; 
   }
@@ -33,25 +30,16 @@ async function cxnDB(){
 }
 
 
-// app.get('/', (req, res) => {
-//   //res.send('Hello World This is Barry 3! <br/> <a href="mongo">mongo</a>');
-
-//   res.render('index'); 
-
-// })
 
 app.get('/', async (req, res) => {
 
   let result = await cxnDB().catch(console.error); 
 
-  // console.log("get/: ", result);
 
   res.render('index', {  drinkData : result })
 })
 
 app.get('/mongo', async (req, res) => {
-
-  // res.send("check your node console, bro");
 
   let result = await cxnDB().catch(console.error); 
 
@@ -61,26 +49,10 @@ app.get('/mongo', async (req, res) => {
 
 })
 
-// app.get('/create', async (req, res) => {
-
-//   //get data from the form 
-
-//   console.log("in get to slash update:", req.query.ejsFormName); 
-//   myName = req.query.ejsFormName; 
-
-//   //update in the database. 
-//   client.connect; 
-//   const collection = client.db("chillAppz").collection("drinkz");
-//   await collection.insertOne({ 
-//     drink_name: "coldiessss from the render app"
-// })
-
-// })
 
 app.post('/addDrink', async (req, res) => {
 
   try {
-    // console.log("req.body: ", req.body) 
     client.connect; 
     const collection = client.db("chillAppz").collection("drinkz");
     await collection.insertOne(req.body);
